09fbe37a884303c93f522abf900b7b51657fc461
Duc <duc@hello-charles.com>
2021-11-08 18:01:53 +0100

    style iframe

--- src/satellites/NewsletterOptIn.ts
+++ src/satellites/NewsletterOptIn.ts
@@ -1,13 +1,16 @@
 import NewsletterOptIn from "./NewsletterOptIn.svelte";
 
 var iframe = document.createElement("iframe");
-var script = document.currentScript;
 iframe.onload = (ev) => {
   const app = new NewsletterOptIn({
     target: iframe.contentWindow.document.body,
   });
   iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
+  iframe.style.border = "none";
+  iframe.style.width = "100%";
 };
+
+var script = document.currentScript;
 script.parentNode.insertBefore(iframe, script);
 
 // const app = new NewsletterOptIn({
