5fe776f82921aba870136fb8ef2177539aff699e
Duc <duc@hello-charles.com>
2021-11-08 17:48:59 +0100

    try iframe

--- src/satellites/NewsletterOptIn.ts
+++ src/satellites/NewsletterOptIn.ts
@@ -2,10 +2,15 @@ import NewsletterOptIn from "./NewsletterOptIn.svelte";
 
 var iframe = document.createElement("iframe");
 var script = document.currentScript;
+iframe.onload = (ev) => {
+  const app = new NewsletterOptIn({
+    target: iframe.contentWindow.document.body,
+  });
+};
 script.parentNode.insertBefore(iframe, script);
 
-const app = new NewsletterOptIn({
-  target: iframe,
-});
+// const app = new NewsletterOptIn({
+//   target: iframe,
+// });
 
-export default app;
+// export default app;
